277a704aa806d22143cdfee2df2304c75601064d
Ashley <ashleyviola@Ashleys-MacBook-Pro.local>
2021-11-10 20:29:42 -0500

    create dynamic search functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,17 @@
-let citySearchInputEl = "#search-btn";
+let searchFormEl = "#city-search-form";
+let searchInputEl = "#search-btn";
 // fetch weather information 
 
 let getCityWeather = function(cityName){
     // execute a current weather get request from open weather api
     let apiKey = "4ac5ffd5c7ed9414f0435b0f9c15caa7";
-    let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=detroit&appid="+apiKey;
+    let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q="+ cityName +"&appid=" + apiKey;
 
     //make request to url 
-    fetch(apiUrl);
+    fetch(apiUrl)
+    .then(function(response){
+        response.json().then(function(data){
+            console.log(data);
+        });
+    });
 };
